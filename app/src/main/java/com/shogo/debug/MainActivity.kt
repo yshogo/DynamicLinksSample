faa439a7d14d1df690154ea90bb2846712eb9fd2
yshogo <superman199323@gmail.com>
2020-04-20 18:45:20 +0900

    URLを生成するように修正

--- app/src/main/java/com/shogo/debug/MainActivity.kt
+++ app/src/main/java/com/shogo/debug/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.shogo.debug
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.dynamiclinks.DynamicLink
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
+import com.shogo.debug.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val dynamicLink = FirebaseDynamicLinks.getInstance().createDynamicLink()
+            .setLink(Uri.parse("https://shogogeek.com/test"))
+            .setDomainUriPrefix("https://shogogeek.com")
+            .setAndroidParameters(DynamicLink.AndroidParameters.Builder().build())
+            .setIosParameters(DynamicLink.IosParameters.Builder("com.example.ios").build())
+            .buildDynamicLink()
+
+        val dynamicLinkUri = dynamicLink.uri
+
+        binding.generatedUrl.visibility = View.VISIBLE
+        binding.generatedUrl.text = dynamicLinkUri.toString()
+
+        val mManager: ClipboardManager = applicationContext.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        binding.generatedUrl.setOnClickListener {
+            mManager.setPrimaryClip(ClipData.newPlainText("", dynamicLinkUri.toString()))
+            Toast.makeText(this, "クリップボードにコピーしました", Toast.LENGTH_LONG).show()
+        }
     }
 }
